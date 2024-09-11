584ba65e6da911385e0db9f77b2bc065e426eda8
Martin Porks <mproksik@gmail.com>
2024-09-11 18:17:58 +0200

    fix: urls with version support

--- constants.py
+++ constants.py
@@ -6,19 +6,19 @@ ZENODO_URL = "https://zenodo.org/records/13749348/files"
 
 DATA = {
     "HUMAN": {
-        "DATASET": f"{ZENODO_URL}/portal_human_v1.1.h5ad",
+        "DATASET": f"{ZENODO_URL}/portal_human_v{VERSION}.h5ad",
         "DEGS": {
-            "CT": f"{ZENODO_URL}/human_degs_ct.feather",
-            "STAGE": f"{ZENODO_URL}/human_degs_stage.feather"
+            "CT": f"{ZENODO_URL}/human_degs_ct_v{VERSION}.feather",
+            "STAGE": f"{ZENODO_URL}/human_degs_stage_v{VERSION}.feather"
         },
-        "SHAP": f"{ZENODO_URL}/human_SHAP.feather",
+        "SHAP": f"{ZENODO_URL}/human_SHAP_v{VERSION}.feather",
     },
     "MOUSE": {
-        "DATASET": f"{ZENODO_URL}/portal_mouse_v1.1.h5ad",
+        "DATASET": f"{ZENODO_URL}/portal_mouse_v{VERSION}.h5ad",
         "DEGS": {
-            "CT": f"{ZENODO_URL}/mouse_degs_ct.feather",
-            "STAGE": f"{ZENODO_URL}/mouse_degs_stage.feather",
+            "CT": f"{ZENODO_URL}/mouse_degs_ct_v{VERSION}.feather",
+            "STAGE": f"{ZENODO_URL}/mouse_degs_stage_v{VERSION}.feather",
         },
-        "SHAP": f"{ZENODO_URL}/mouse_SHAP.feather",
+        "SHAP": f"{ZENODO_URL}/mouse_SHAP_v{VERSION}.feather",
     },
 }
